7a660031ea1d4dec5a2ff259d3c6fc9a103d3852
ThomG <t.galdi@gmail.com>
2023-03-03 15:24:47 -0500

    performance aware part 1

---
+++ projects/part1/move.cpp
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <bitset>
+#include <iostream>
+
+void execute( char, char );
+const char getbits( char, int, int );
+const char* opLookup( const char );
+const char* registerLookup( const char, const char );
+const char* displacementLookup( const char, const char, const char );
+
+int main( int argc, char** argv )
+{
+    int exitCode = 0;
+
+    // ensure cmd arg was provided
+    if( argc < 1 )
+    {
+        fprintf( stderr, "Usage: %s <filename>\n", argv[0] );
+        exit( -1 );
+    }
+
+    char* fileName = argv[1];
+    FILE* file = fopen( argv[1], "rb" );
+
+    // ensure file is valid
+    if( file == nullptr )
+    {
+        fprintf( stderr, "ERROR: Cannot open file %s\n", fileName );
+        exit( 1 );
+    }
+
+    // calculate file size
+    fseek( file, 0, SEEK_END );
+    long fileSize = ftell( file );
+    rewind( file );
+
+    if( (fileSize % 2) != 0 )
+    {
+        fprintf( stderr, "ERROR: File does not conform to word size instructions %s\n", fileName );
+        exit( 2 );
+    }
+
+    // allocate memory for file
+    char* buffer = (char*)malloc( sizeof( char ) * fileSize );
+
+    // ensure system memory was allocated
+    if( buffer == nullptr )
+    {
+        fprintf( stderr, "ERROR: Cannot allocate space for buffer.\n" );
+        fclose( file );
+        exit( 3 );
+    }
+
+    // read file into buffer
+    long result = fread( buffer, 1, fileSize, file );
+    fclose( file );
+
+    // ensure file was read
+    if( result != fileSize )
+    {
+        fprintf( stderr, "ERROR: Error reading file.\n" );
+        exit( 4 );
+    }
+
+    char* opTablePtr = buffer;
+    char* endPtr = buffer + fileSize;
+
+    // read op codes
+    while( opTablePtr < endPtr )
+    {
+        char hi = *opTablePtr++;
+        char lo = *opTablePtr++;
+        execute( hi, lo );
+    }
+
+    exit( 0 );
+}
+
+void execute( char hi, char lo )
+{
+    // std::cout << std::bitset<8>( hi ) << std::endl;
+    // std::cout << std::bitset<8>( lo ) << std::endl;
+
+    const char opCode = getbits( hi, 7, 6 );
+    const char d = getbits( hi, 1, 1 );
+    const char w = getbits( hi, 0, 1 );
+    const char mod = getbits( lo, 7, 2 );
+    const char reg = getbits( lo, 5, 3 );
+    const char rm = getbits( lo, 2, 3 );
+
+    const char* op = opLookup( opCode );
+
+    const char* reg1 = d
+        ? registerLookup( reg, w )
+        : displacementLookup( mod, rm, w );
+
+    const char* reg2 = d
+        ? displacementLookup( mod, rm, w )
+        : registerLookup( reg, w );
+
+    printf( "%s %s, %s\n", op, reg1, reg2 );
+}
+
+const char getbits( char x, int p, int n )
+{
+    return (x >> (p + 1 - n)) & ~(~0 << n);
+}
+
+enum opCodes
+{
+    no_op,
+    mov,
+    op_count
+};
+static const char opCodeTable[op_count] = { 0b0, 0b00100010 };
+static const char* opNameTable[op_count] = { "no_op\0", "mov\0" };
+const char* opLookup( const char op )
+{
+    int opCode = no_op;
+
+    const char* opTablePtr = opCodeTable;
+    while( opCode < op_count )
+    {
+        if( opTablePtr[opCode] == op )
+            break;
+
+        ++opCode;
+    }
+
+    return opNameTable[opCode];
+}
+
+enum registers
+{
+    AL,
+    CL,
+    DL,
+    BL,
+    AH,
+    CH,
+    DH,
+    BH,
+    reg_count
+};
+static const char* regNameTable[2][reg_count] = { { "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh" },
+                                                { "ax", "cx", "dx", "bx", "sp", "bp", "si", "di" } };
+const char* registerLookup( const char reg, const char w )
+{
+    if( reg >= reg_count )
+        return "invalid register\0";
+
+    return regNameTable[w][reg];
+}
+
+enum mode
+{
+    mem_mode,
+    byte_mode,
+    word_mode,
+    reg_mode,
+    mode_count
+};
+const char* displacementLookup( const char mod, const char rm, const char w )
+{
+    switch( mod )
+    {
+        case reg_mode:
+            return registerLookup( rm, w );
+        default:
+            return "mode not implemented\0";
+    }
+}
